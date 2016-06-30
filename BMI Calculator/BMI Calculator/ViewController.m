8dae3040b120009b0abb7bb175ca0c8d15a11ae2
Luca Koval <lucakoval@me.com>
2016-06-30 11:42:09 -0400

    Well, that didn't work
    yee

--- BMI Calculator/BMI Calculator/ViewController.m
+++ BMI Calculator/BMI Calculator/ViewController.m
@@ -23,7 +23,21 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = RGB(233, 233, 233);
     self.bmiLabel.textAlignment = NSTextAlignmentCenter;
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeCenterX relatedBy:0 toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
+    //CGRect frameView = self.view.frame;
+    //CGRect frame = self.bmiLabel.frame;
+    //frame.origin.x = frameView.origin.x/2;
+    //frame.origin.y = frameView.origin.y/2;
+    //CGFloat selfHeight = self.view.bounds.size.height;
+    //CGFloat selfWidth = self.view.bounds.size.width;
+    //CGFloat boxHeight=box.bounds.size.height;
+    //CGFloat boxWidth=box.bounds.size.width;
+    //NSLog(@"%f", selfWidth);
+    //NSLog(@"%f", selfHeight);
+    //frame.origin.y = self.view.bounds.size.height/2;
+    //frame.origin.x = self.view.bounds.size.width/2;
+    //frame.origin.y = [[UIScreen mainScreen] bounds].size.height/2;
+    //frame.origin.x = [[UIScreen mainScreen] bounds].size.width/2;
+    //self.bmiLabel.frame = frame;
 }
 
 - (void)didReceiveMemoryWarning {
